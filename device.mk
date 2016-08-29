c6a203267c7fd0b01dc2d9e0a9a1b3f73dfcef2b
Nepo Rood <neporood@users.noreply.github.com>
2016-08-29 06:39:22 -0500

    Remove Some, Add Some
    Removed unneeded ramdisk contensts. Added dalvik heap from frameworks and screen density setting

--- device.mk
+++ device.mk
@@ -15,27 +15,22 @@ TARGET_SCREEN_WIDTH := 1366
 
 # Boot Ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/root/res/images/charger/battery_fail.png:root/res/images/charger/battery_fail.png \
-    $(LOCAL_PATH)/root/res/images/charger/battery_scale.png:root/res/images/charger/battery_scale.png \
     $(LOCAL_PATH)/root/sbin/init_parttion.sh:root/sbin/init_parttion.sh \
     $(LOCAL_PATH)/root/config_mem.ini:root/config_mem.ini \
     $(LOCAL_PATH)/root/disp.ko:root/disp.ko \
     $(LOCAL_PATH)/root/fstab.sun8i:root/fstab.sun8i \
-    $(LOCAL_PATH)/root/init:root/init \
-    $(LOCAL_PATH)/root/init.common.rc/:root/init.common.rc \
-    $(LOCAL_PATH)/root/init.environ.rc:root/init.environ.rc \
     $(LOCAL_PATH)/root/init.rc:root/init.rc \
     $(LOCAL_PATH)/root/init.recovery.sun8i.rc:root/init.recovery.sun8i.rc \
     $(LOCAL_PATH)/root/init.sun8i.common.rc:root/init.sun8i.common.rc \
     $(LOCAL_PATH)/root/init.sun8i.rc:root/init.sun8i.rc \
     $(LOCAL_PATH)/root/init.sun8i.usb.rc:root/init.sun8i.usb.rc \
-    $(LOCAL_PATH)/root/init.trace.rc:root/init.trace.rc \
     $(LOCAL_PATH)/root/init.usb.rc:root/init.usb.rc \
-    $(LOCAL_PATH)/root/init.zygote32.rc:root/init.zygote32.rc \
     $(LOCAL_PATH)/root/nand.ko:root/nand.ko \
     $(LOCAL_PATH)/root/sunxi_tr.ko:root/sunxi_tr.ko \
     $(LOCAL_PATH)/root/ueventd.rc:root/ueventd.rc \
     $(LOCAL_PATH)/root/ueventd.sun8i.rc:root/ueventd.sun8i.rc
+
+$(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
 # Properties
 ADDITIONAL_DEFAULT_PROPERTIES += \
@@ -47,10 +42,13 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/configs/libsoftwinner-ril-5.0.so:system/lib64/libsoftwinner-ril-5.0.so
 
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := 160dpi
+
 # Wifi
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf
-
