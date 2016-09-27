a44c07f2168867695136ee5dc49edb2b6646ab52
neporood <neporood@live.com>
2016-09-27 11:05:07 -0500

    Staging AOSP

--- aosp.mk
+++ aosp.mk
@@ -7,7 +7,7 @@ $(call inherit-product, device/TabletExpress/X10/aosp_X10.mk)
 # $(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_RELEASE_NAME := Dragon Touch X10
-PRODUCT_NAME := X10
+PRODUCT_NAME := aosp_X10
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_DISPLAY_ID='X10-userdebug $(PLATFORM_VERSION) $(BUILD_ID) $(BUILD_NUMBER) test-keys' \
--- aosp_X10.mk
+++ aosp_X10.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/TabletExpress/X10/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X10
-PRODUCT_NAME := full_X10
+PRODUCT_NAME := aosp_X10
 PRODUCT_BRAND := Allwinner
 PRODUCT_MODEL := X10
 PRODUCT_MANUFACTURER := TabletExpress
