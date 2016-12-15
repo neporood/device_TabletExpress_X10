930119a2fac5203604b3bae02386aa73bb6e95e2
Nepo Rood <neporood@users.noreply.github.com>
2016-12-15 04:26:39 -0600

    Update and rename bliss.mk to cm.mk

--- cm.mk
+++ cm.mk
@@ -4,11 +4,11 @@ $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
 $(call inherit-product, device/TabletExpress/X10/full_X10.mk)
 
 # Inherit from bliss vendor some common stuff
-$(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_RELEASE_NAME := Dragon Touch X10
-PRODUCT_NAME := bliss_X10
+PRODUCT_NAME := cm_X10
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_DISPLAY_ID='X10-userdebug $(PLATFORM_VERSION) $(BUILD_ID) $(BUILD_NUMBER) test-keys' \
-    TARGET_BUILD_FLAVOR=bliss_X10-userdebug
+    TARGET_BUILD_FLAVOR=cm_X10-userdebug
