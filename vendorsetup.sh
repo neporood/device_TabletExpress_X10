9dfe2820f55b72713447694fdb4b25f6d333ab3f
Nepo Rood <neporood@users.noreply.github.com>
2016-12-15 04:28:20 -0600

    Lunch CM device

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo bliss_X10-userdebug
+add_lunch_combo cm_X10-userdebug
